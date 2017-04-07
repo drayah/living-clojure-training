31569198e3dc91442ca2ece14d289f8bfb9b398a
Giovanni Martina <gio@popado.co>
2017-04-07 20:28:57 -0300

    Add more tests

--- test/problems/core_test.cljs
+++ test/problems/core_test.cljs
@@ -34,6 +34,12 @@
   (is (= 1 (c/gcd 5 7)))
   (is (= 33 (c/gcd 1023 858))))
 
+(deftest euclid-gcd
+  (is (= 2 (c/euclid-gcd 2 4)))
+  (is (= 5 (c/euclid-gcd 10 5)))
+  (is (= 1 (c/euclid-gcd 5 7)))
+  (is (= 33 (c/euclid-gcd 1023 858))))
+
 (deftest pwr
   (is (= 256 ((c/pwr 2) 16) ((c/pwr 8) 2)))
   (is (= [1 8 27 64] (map (c/pwr 3) [1 2 3 4])))
@@ -46,6 +52,101 @@
                 (into #{} (range 30)))))))
 
 (deftest symmetric-difference
-  (is (= #{2 4 6 7} (c/symmetric-difference #{1 2 3 4 5 6} #{1 3 5 7})))
+  (is (= #{2 4 6 7} 
+         (c/symmetric-difference 
+           #{1 2 3 4 5 6} 
+           #{1 3 5 7})))
   (is (= #{:a :b :c} (c/symmetric-difference #{:a :b :c} #{})))
-  (is (= #{4 5 6} (c/symmetric-difference #{} #{4 5 6}))))
+  (is (= #{4 5 6} (c/symmetric-difference #{} #{4 5 6})))
+  (is (= #{[1 2] [3 4]} 
+         (c/symmetric-difference
+           #{[1 2] [2 3]}
+           #{[2 3] [3 4]}))))
+
+(deftest lcm
+  (is (= 6 (c/lcm 2 3)))
+  (is (= 105 (c/lcm 5 3 7))))
+
+(deftest pascal
+  (is (= [1] (c/pascal 1)))
+  (is (= [1 10 45 120 210 252 210 120 45 10 1]
+         (c/pascal 11))))
+
+(deftest tree?
+  (is (= false
+         (c/tree? '(:a nil ()))))
+  (is (= true
+         (c/tree? '(:a (:b nil nil) nil))))
+  (is (= false
+         (c/tree? '(:a (:b nil nil)))))
+  (is (= true 
+         (c/tree? [1 nil [2 [3 nil nil] [4 nil nil]]])))
+  (is (= false
+         (c/tree? [1 [2 nil nil] [3 nil nil] [4 nil nil]])))
+  (is (= true
+         (c/tree? [1 [2 [3 [4 nil nil] nil] nil] nil])))
+  (is (= false
+         (c/tree? [1 [2 [3 [4 false nil] nil] nil] nil]))))
+
+(deftest sym-tree?
+  (is (= true
+         (c/symmetric-tree? '(:a (:b nil nil) (:b nil nil)))))
+  (is (= false
+         (c/symmetric-tree? '(:a (:b nil nil) nil))))
+  (is (= false
+         (c/symmetric-tree? '(:a (:b nil nil) (:c nil nil)))))
+  (is (= true
+         (c/symmetric-tree? [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+                               [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]]))))
+
+(deftest flip
+  (is (= 3
+         ((c/flip nth) 2 [1 2 3 4 5])))
+  (is (= true
+         ((c/flip >) 7 8)))
+  (is (= 4
+         ((c/flip quot) 2 8)))
+  (is (= [1 2 3]
+         ((c/flip take) [1 2 3 4 5] 3))))
+
+(deftest rotate
+  (is (= '(3 4 5 1 2)
+         (c/rotate 2 [1 2 3 4 5])))
+  (is (= '(4 5 1 2 3)
+         (c/rotate -2 [1 2 3 4 5])))
+  (is (= '(2 3 4 5 1)
+         (c/rotate 6 [1 2 3 4 5])))
+  (is (= '(:b :c :a)
+         (c/rotate 1 '(:a :b :c))))
+  (is (= '(:c :a :b)
+         (c/rotate -4 '(:a :b :c)))))
+
+(deftest reverse-interleave
+  (is (= '((1 3 5) (2 4 6))
+         (c/reverse-interleave [1 2 3 4 5 6] 2)))
+  (is (= '((0 3 6) (1 4 7) (2 5 8))
+         (c/reverse-interleave (range 9) 3)))
+  (is (= '((0 5) (1 6) (2 7) (3 8) (4 9)
+           (c/reverse-interleave (range 10) 5)))))
+
+(deftest split
+  (is (= #{[1 2 3] [:a :b :c]}
+         (set (c/split-by-type [1 :a 2 :b 3 :c]))))
+  (is (= #{[:a :b] ["foo" "bar"]}
+         (set (c/split-by-type [:a "foo" "bar" :b]))))
+  (is (= #{[[1 2] [3 4]] [:a :b] [5 6]}
+         (set (c/split-by-type [[1 2] :a [3 4] 5 6 :b])))))
+
+(deftest primes
+  (is (= [2 3]
+         (c/primes 2)))
+  (is (= [2 3 5 7 11]
+         (c/primes 5)))
+  (is (= 541
+         (last (c/primes 100)))))
+
+(deftest anagrams
+  (is (= #{#{"meat" "team" "mate"}}
+         (c/anagrams ["meat" "mat" "team" "mate" "eat"])))
+  (is (= #{#{"veer" "ever"} #{"lake" "kale"} #{"mite" "item"}}
+         (c/anagrams ["veer" "lake" "item" "kale" "mite" "ever"]))))
